dcf4f0a7857b01cfa2a1d045d29b17514f919523
coma333 <coma333ryu@gmail.com>
2024-07-26 14:44:48 +0900

    로또 번호 생성 수정
    5셋트 출력된 번호를 제외한 5셋트 추가 생성

--- main.go
+++ main.go
@@ -8,21 +8,44 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	numberList := make([]int, 0)
+	tempNumberList := make([]int, 0)
 
-	for len(numberList) < 6 {
-		number := rand.Intn(45) + 1
+	for i := 0; i < 5; i++ {
+		numberList := make([]int, 0)
+		for len(numberList) < 6 {
+			number := rand.Intn(45) + 1
 
-		if !removeDuplicateNumber(numberList, number) {
-			numberList = append(numberList, number)
+			if !removeDuplicateNumber(numberList, number) {
+				numberList = append(numberList, number)
+			}
 		}
+
+		// 오름차순 정렬
+		slices.Sort(numberList)
+		fmt.Println("Number List:", numberList)
+
+		tempNumberList = append(tempNumberList, numberList...)
+
 	}
 
-	// 오름차순 정렬
-	slices.Sort(numberList)
-	fmt.Println("Number List:", numberList)
+	uniqueTempNumberList := removeDuplicates(tempNumberList)
+	// fmt.Println("Unique Temp Number List:", uniqueTempNumberList)
+
+	for i := 0; i < 5; i++ {
+		newNumberList := make([]int, 0)
+		for len(newNumberList) < 6 {
+			newNumber := rand.Intn(45) + 1
+
+			if !removeDuplicateNumber(uniqueTempNumberList, newNumber) && !removeDuplicateNumber(newNumberList, newNumber) {
+				newNumberList = append(newNumberList, newNumber)
+			}
+		}
+
+		slices.Sort(newNumberList)
+		fmt.Println("New Number List:", newNumberList)
+	}
 
 }
 
@@ -33,4 +56,18 @@ func removeDuplicateNumber(targetNumberList []int, checkNumber int) bool {
 		}
 	}
 	return false
+}
+
+func removeDuplicates(numbers []int) []int {
+	seen := make(map[int]bool)
+	uniqueNumbers := make([]int, 0)
+
+	for _, number := range numbers {
+		if !seen[number] {
+			seen[number] = true
+			uniqueNumbers = append(uniqueNumbers, number)
+		}
+	}
+
+	return uniqueNumbers
 }
